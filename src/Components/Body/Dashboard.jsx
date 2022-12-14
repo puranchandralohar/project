5eb2f37a60e5b497153d922150b8d1802d9a0166
puranchandralohar <clpuran95@gmail.com>
2022-12-14 14:58:06 +0530

    Added

--- src/Components/Body/Dashboard.jsx
+++ src/Components/Body/Dashboard.jsx
@@ -25,6 +25,28 @@ export const Dashboard = () => {
            <img src={user.picture} />
           <button className='btn signout' onClick={(e) => handleSignout(e)}>Sign Out</button>
          </div>
+         <div className="sidebar">
+            <div className="sidebar_title">
+              <h2>Setup Class</h2>
+            </div>
+            <div className="sidebar_tasks">
+              <div className="input_elements">
+                <label htmlFor="class_name">Class Name</label>
+                <input type="text" name="class_name" id="class_name" />
+              </div>
+            
+              <div className="input_elements">
+                <label htmlFor="class_time">Timing</label>
+                <input type="datetime-local" name="class_time" id="class_time" />
+              </div>
+
+              <div className="input_elements">
+                <label htmlFor="class_cost">Cost</label>
+                <input type="text" name="class_cost" id="class_cost" />
+              </div>
+              
+            </div>
+         </div>
     </div>
   )
 }
